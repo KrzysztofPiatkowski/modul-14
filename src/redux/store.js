b739ccbc3038c437b3b1564c1871c8e0c27654ab
Krzysztof Piatkowski <krzysztofpiatkowski1987@gmail.com>
2025-04-26 18:27:19 +0200

    Refactor card filtering logic into selector and utility function

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,10 @@
 import { createStore } from 'redux';
 import initialState from './initialState';
 import shortid from 'shortid';
+import strContains from '../utils/strContains';
+
+  export const getFilteredCards = ({ cards, searchString }, columnId) => cards
+  .filter(card => card.columnId === columnId && strContains(card.title, searchString));
 
 const reducer = (state, action) => {
   switch(action.type) {
